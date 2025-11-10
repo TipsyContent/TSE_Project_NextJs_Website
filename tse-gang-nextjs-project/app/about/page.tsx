4d1e848e7495d644b8cc5481c2e10a3498f8f2ec
Hebbeltje <susannesolheim@hotmail.com>
2025-11-10 13:25:34 +0100

    chore(About) add blank aboutSocial component

--- tse-gang-nextjs-project/app/about/page.tsx
+++ tse-gang-nextjs-project/app/about/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 //test
 export default function About() {
   return (
-    <div className="flex items-center justify-center p-10 bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex items-center justify-center p-10">
       <div className="flex flex-col items-center text-center">
-        <h1 className="text-4xl font-bold">About Us</h1>
-        <div className="mt-4 max-w-2xl text-lg text-gray-700 dark:text-gray-300">
+        <h1 className="text-3xl font-bold">About Us</h1>
+        <div className="mt-4 max-w-2xl text-lg">
           <p>We are TSE and stuff and we play Valorant.</p>
         </div>
       </div>
---
+++ tse-gang-nextjs-project/components/aboutSocials.tsx
@@ -0,0 +1,7 @@
+export function aboutSocials() {
+  return (
+    <div className="about-socials flex justify-center gap-5 flex-wrap mt-4">
+      <a href="#" className="social-link"></a>
+    </div>
+  );
+}
