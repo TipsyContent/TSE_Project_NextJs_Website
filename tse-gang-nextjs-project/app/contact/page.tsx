cf2a4c0f80e6ac522339a8d6d10aabf311dfc824
JosephineGormsen <josephinegormsen@hotmail.com>
2025-11-12 07:38:40 +0100

    Removed comment

--- tse-gang-nextjs-project/app/contact/page.tsx
+++ tse-gang-nextjs-project/app/contact/page.tsx
@@ -1,66 +1,6 @@
 import Link from "next/link";
 import FormContacts from "@/components/ui/formContact";
 
-// export default function ContactPage() {
-//   return (
-    
-//     <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-50 px-6 text-center font-sans dark:bg-black">
-//       <p className="text-gray-700 dark:text-gray-200">
-//       <FormContacts></FormContacts>
-//         Before you reach out, feel free to review how we look after your data:
-//       </p>
-//       <Link
-//         href="/privacy"
-//         className="text-base font-medium text-blue-600 underline-offset-4 hover:underline dark:text-blue-400"
-//       >
-//         Privacy Policy
-//       </Link>
-//     </div>
-//   );
-// }
-
-// export default function ContactPage() {
-//   return (
-//     <div className="min-h-screen bg-zinc-50 px-6 py-12 font-sans dark:bg-black">
-//       <div className="mx-auto max-w-6xl">
-//         <FormContacts />
-        
-//         <div className="mt-8 text-center">
-//           <p className="text-gray-700 dark:text-gray-200">
-//             Before you reach out, feel free to review how we look after your data:
-//           </p>
-//           <Link
-//             href="/privacy"
-//             className="text-base font-medium text-blue-600 underline-offset-4 hover:underline dark:text-blue-400"
-//           >
-//             Privacy Policy
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default function ContactPage() {
-//   return (
-//     <div className="min-h-screen bg-zinc-50 px-6 py-12 font-sans dark:bg-black">
-//       <FormContacts />
-      
-//       <div className="mt-8 text-center">
-//         <p className="text-gray-700 dark:text-gray-200">
-//           Before you reach out, feel free to review how we look after your data:
-//         </p>
-//         <Link
-//           href="/privacy"
-//           className="text-base font-medium text-blue-600 underline-offset-4 hover:underline dark:text-blue-400"
-//         >
-//           Privacy Policy
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
 export default function ContactPage() {
   return (
    <div className="min-h-screen bg-white font-sans dark:bg-black">
